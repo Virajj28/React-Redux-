9d23879cd383453de82a30772f3d411d1dd87445
Virajj28 <viraj28j@gmail.com>
2022-03-13 17:24:39 +0530

    reducer

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function buyCake() {
         info: 'First redux action'
     }
 }
+
+// (previousState, action) => newState
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case Buy_Cake: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state;
+    }
+}
