f3243bb70a415641662c3868ea093bc0775be8dd
Virajj28 <viraj28j@gmail.com>
2022-04-17 23:55:50 +0530

    combine reducers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import redux from 'redux'; with file proj.
 const redux = require('redux') // node module
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE= 'BUY_CAKE';
 const BUY_ICECREAM= 'BUY_ICECREAM'
@@ -22,28 +23,62 @@ function buyIceCream() {
 
 // (previousState, action) => newState
 
-const initialState = {
-    numOfCakes: 10,
-    numOfIceCreams: 20,
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20,
+// }
+
+const initialCakeState = {
+    numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+
+// const reducer = (state = initialState, action) => {
+//     switch(action.type) {
+//         case BUY_CAKE: return {
+//             ...state,
+//             numOfCakes: state.numOfCakes - 1
+//         }
+        
+//         case BUY_ICECREAM: return {
+//             ...state,
+//             numOfIceCreams: state.numOfIceCreams - 1
+//         }
+
+//         default: return state;
+//     }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
+    switch(action.type) {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
         }
-        
-        case BUY_ICECREAM: return {
-            ...state,
-            numOfIceCreams: state.numOfIceCreams - 1
-        }
-
         default: return state;
     }
 }
 
-const store = createStore(reducer); // 1st responsibility
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
+        default: return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+
+const store = createStore(rootReducer);
+// const store = createStore(reducer); // Just for single reducers 1st responsibility
 console.log('Intial State', store.getState()) //2nd
 const unsubscribe = store.subscribe(() => console.log('Update State', store.getState())) //4th
 store.dispatch(buyCake()) //3rd
